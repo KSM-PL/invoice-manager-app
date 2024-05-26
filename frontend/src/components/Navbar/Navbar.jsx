7f2bfd6c17270b909be8919e89bfb45f9546cf37
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-26 20:25:32 +0200

    🎨add backdrop blur to navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     // const auth = useAuthUser();
 
     return (
-        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full justify-between">
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full justify-between backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 <Link
                     href="#"
