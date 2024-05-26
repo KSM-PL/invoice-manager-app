f322aa5f55b6e2921d143391d692cbf379ca790d
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-26 20:24:00 +0200

    ✨create invoice in and out pages

--- frontend/src/pages/InvoiceOut/InvoiceOut.jsx
+++ frontend/src/pages/InvoiceOut/InvoiceOut.jsx
@@ -24,12 +24,13 @@ import {
     ChevronLeftIcon,
     ChevronRightIcon,
     DotsHorizontalIcon,
+    ValueNoneIcon,
     CheckIcon,
 } from "@radix-ui/react-icons";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useToast } from "@/components/ui/use-toast";
 
-const InvoiceIn = () => {
+const InvoiceOut = () => {
 	const authHeader = useAuthHeader();
     const { toast } = useToast();
 
@@ -43,7 +44,7 @@ const InvoiceIn = () => {
     const fetchInvoices = async () => {
         setLoading(true);
 
-        fetch(`http://localhost:8080/api/v1/invoices/?pageNumber=${(currentPage - 1)}&pageSize=${pageSize}&type=in`, {
+        fetch(`http://localhost:8080/api/v1/invoices/?pageNumber=${(currentPage - 1)}&pageSize=${pageSize}&type=out`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json', 
@@ -96,72 +97,68 @@ const InvoiceIn = () => {
     };
 
     return (
-        <MainContainer type="invoice-in">
+        <MainContainer type="invoice-out">
             <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
                 <Table>
                     {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
                     <TableHeader>
                         <TableRow>
                             <TableHead className="w-fit text-nowrap">Invoice ID</TableHead>
-                            <TableHead className="w-fit text-nowrap">Author name</TableHead>
-                            <TableHead className="w-fit text-nowrap">Author email</TableHead>
+                            <TableHead className="w-fit text-nowrap">Recipient name</TableHead>
+                            <TableHead className="w-fit text-nowrap">Recipient email</TableHead>
                             <TableHead className="w-full text-nowrap">Description</TableHead>
                             <TableHead className="w-fit text-right text-nowrap">Amount</TableHead>
-                            <TableHead className="w-fit text-right text-nowrap"></TableHead>
+                            {/* <TableHead className="w-fit text-right text-nowrap"></TableHead> */}
                         </TableRow>
                     </TableHeader>
                     <TableBody>
 
                         {loading ? (
                             <>
-                            <TableRow>
-                                <TableCell><Skeleton className="w-[180px] h-[30px] my-1" /></TableCell>
-                                <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[90px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-full h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell><Skeleton className="w-[180px] h-[30px] my-1" /></TableCell>
-                                <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-full h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
-                                <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
-                            </TableRow>
+                                <TableRow>
+                                    <TableCell><Skeleton className="w-[180px] h-[30px] my-1" /></TableCell>
+                                    <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-[90px] h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-full h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
+                                    {/* <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell> */}
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell><Skeleton className="w-[180px] h-[30px] my-1" /></TableCell>
+                                    <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-[50px] h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-full h-[30px]" /></TableCell>
+                                    <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell>
+                                    {/* <TableCell><Skeleton className="w-[40px] h-[30px]" /></TableCell> */}
+                                </TableRow>
                             </>
 
-                        ) : (
-                            invoices && invoices.map((invoice, i) => {
-                                return (
-                                    <TableRow key={`tablerow-${invoice.id}`}>
-                                        <TableCell className="font-medium">{invoice.id}</TableCell>
-                                        <TableCell>{invoice.authorFullName}</TableCell>
-                                        <TableCell>{invoice.authorEmail}</TableCell>
-                                        <TableCell>{invoice.name}</TableCell>
-                                        <TableCell>${invoice.amount}</TableCell>
-                                        <TableCell>
-                                            <Button variant="outline" className="gap-1 p-2">
-                                                <CheckIcon />
-                                                Pay
-                                            </Button>
-                                        </TableCell>
-                                        
-                                    </TableRow>
-                                )
-                            })
+                        ) : (invoices && (
+                                invoices.map((invoice, i) => {
+                                    return (
+                                        <TableRow key={`tablerow-${invoice.id}`}>
+                                            <TableCell className="font-medium">{invoice.id}</TableCell>
+                                            <TableCell>{invoice.recipientFullName}</TableCell>
+                                            <TableCell>{invoice.recipientEmail}</TableCell>
+                                            <TableCell>{invoice.name}</TableCell>
+                                            <TableCell>${invoice.amount}</TableCell>
+                                            {/* <TableCell>
+                                                <Button variant="outline" className="gap-1 p-2">
+                                                    <CheckIcon />
+                                                    Pay
+                                                </Button>
+                                            </TableCell> */}
+                                            
+                                        </TableRow>
+                                    )
+                                })
+                            )
+                                
                         )}
-                        
-
-                            {/* <TableCell className="font-medium">???</TableCell>
-                            <TableCell>??</TableCell>
-                            <TableCell>??</TableCell>
-                            <TableCell>???</TableCell>
-                            <TableCell>$??</TableCell> */}
                         
                     </TableBody>
                 </Table>
+                
 
                 {totalPages > 1 && (
                     <div className="flex flex-row justify-center items-center gap-3">
@@ -188,4 +185,4 @@ const InvoiceIn = () => {
     )
 }
 
-export default InvoiceIn;
+export default InvoiceOut;
