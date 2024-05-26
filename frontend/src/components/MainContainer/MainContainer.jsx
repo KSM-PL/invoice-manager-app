4cce97d239c903983ff40cf74465a61613ef3918
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-26 20:24:26 +0200

    🎨 change font size and width

--- frontend/src/components/MainContainer/MainContainer.jsx
+++ frontend/src/components/MainContainer/MainContainer.jsx
@@ -6,12 +6,13 @@ const MainContainer = ({children, type = "default"}) => {
     return (
         <div className="flex flex-col items-start min-h-screen">
             <Navbar />
+            <div className='absolute left-0 right-0 top-0 -z-10 h-52 bg-gradient-to-b from-primary/10 from-10% ' />
 
             <main className="w-full flex flex-col justify-center items-center">
 
                 {type == "dashboard" ? (
                     <main className="max-w-full w-[1400px] px-10">
-                        <h2 className="text-center scroll-m-20 text-lg font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
+                        <h2 className="text-center scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Dashboard
                         </h2>
                         {children}
@@ -30,7 +31,7 @@ const MainContainer = ({children, type = "default"}) => {
                 ) : null}
 
                 {type == "invoice-out" ? (
-                    <div className="max-w-full w-[700px]">
+                    <div className="max-w-full w-[1400px] px-10">
                         <h2 className="text-center scroll-m-20 text-lg font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Invoice out
                         </h2>
