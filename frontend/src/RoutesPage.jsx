35bfe20e0306ef1366441db0a24e6648b5fa9397
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-27 16:14:17 +0200

    ✨ add route to create-invoice

--- frontend/src/RoutesPage.jsx
+++ frontend/src/RoutesPage.jsx
@@ -10,6 +10,7 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import InvoiceIn from "./pages/InvoiceIn/InvoiceIn";
 import InvoiceOut from "./pages/InvoiceOut/InvoiceOut";
 // import Secure from "./componants/Secure";
+import CreateInvoice from './pages/CreateInvoice/CreateInvoice';
 
 const RoutesPage = () => {
 
@@ -25,6 +26,7 @@ const RoutesPage = () => {
                     <Route path='/' element={<Home/>} />
                     <Route path='/invoice-in' element={<InvoiceIn/>} />
                     <Route path='/invoice-out' element={<InvoiceOut/>} />
+                    <Route path='/create-invoice' element={<CreateInvoice/>} />
 
                     {/* <Route path='/profile/:userId' element={<Profile />} /> */}
                     <Route path='/logout' element={<Logout />} />
