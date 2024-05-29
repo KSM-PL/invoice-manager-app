9d3d49090f74721b543e1c35313b9b8a1cbb9f73
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-30 00:19:52 +0200

    🎨change font size, add description

--- frontend/src/components/MainContainer/MainContainer.jsx
+++ frontend/src/components/MainContainer/MainContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
-const MainContainer = ({children, type = "default"}) => {
+const MainContainer = ({children, type = "default", description = ""}) => {
     return (
         <div className="flex flex-col items-start min-h-screen">
             <Navbar />
@@ -11,40 +11,50 @@ const MainContainer = ({children, type = "default"}) => {
             <main className="w-full flex flex-col justify-center items-center">
 
                 {type == "dashboard" ? (
-                    <main className="max-w-full w-[1400px] px-10">
-                        <h2 className="text-center scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
+                    <div className="max-w-full w-[1400px] px-10">
+                        <h2 className="text-center text-3xl font-extrabold tracking-tight md:text-5xl lg:leading-[1.1] pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Dashboard
                         </h2>
                         {children}
-                    </main>
+                    </div>
                         
                 ) : null}
 
                 {type == "invoice-in" ? (
-                    <div className="max-w-full w-[1400px] px-10">
-                        <h2 className="text-center scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
+                    <div className="max-w-full w-[1400px] px-10 flex flex-col justify-center items-center">
+                        {/* text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1] */}
+                        <h2 className="text-center text-3xl font-extrabold tracking-tight md:text-5xl lg:leading-[1.1] pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Invoice in
                         </h2>
+                        <h3 className="max-w-[750px] text-center text-lg font-light text-foreground mt-2">
+                            {description}
+                        </h3>
                         {children}
                     </div>
                     
                 ) : null}
 
                 {type == "invoice-out" ? (
-                    <div className="max-w-full w-[1400px] px-10">
-                        <h2 className="text-center scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
+                    <div className="max-w-full w-[1400px] px-10 flex flex-col justify-center items-center">
+                        <h2 className="text-center text-3xl font-extrabold tracking-tight md:text-5xl lg:leading-[1.1] pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Invoice out
                         </h2>
+                        <h3 className="max-w-[750px] text-center text-lg font-light text-foreground mt-2">
+                            {description}
+                        </h3>
                         {children}
                     </div>
                     
                 ) : null}
 
                 {type == "create-invoice" ? (
-                    <div className="max-w-full w-[800px] px-10">
-                        <h2 className="text-center scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
+                    <div className="max-w-full w-[800px] px-10 flex flex-col justify-center items-center">
+                        <h2 className="text-center text-3xl font-extrabold tracking-tight md:text-5xl lg:leading-[1.1] pt-4 md:pt-8 [text-shadow:_0_0_5px_rgb(255_255_255_/_100%)]">
                             Create invoice
                         </h2>
+                        <h3 className="max-w-[750px] text-center text-lg font-light text-foreground mt-2">
+                            {description}
+                        </h3>
                         {children}
                     </div>
                     
